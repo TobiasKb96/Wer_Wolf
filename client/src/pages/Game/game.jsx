f2001e5d895926d3e70e030e3b2de0b2e1e310f8
Anna <anna.zierlinger@stud.fh-campuswien.ac.at>
2025-01-19 00:59:01 +0100

    merged

--- client/src/pages/Game/game.jsx
+++ client/src/pages/Game/game.jsx
@@ -1,16 +1,12 @@
-import Join from "../Join/join.jsx";
 import React, {useState, useEffect} from "react";
 import gameController from "./gamelogic/gameController.js";
-import LobbyParticipants from "../../components/LobbyParticipants.jsx";
 import PropTypes from "prop-types";
-import Player from "./gamelogic/Player.js";
 import socket from "../../utils/socket.js";
 import PlayerOverview from "../../components/playerOverview.jsx";
 import Voting from "../../components/voting.jsx";
-import modalOverview from "../../components/modalOverview.jsx";
 import ModalOverview from "../../components/modalOverview.jsx";
-import sunImg from '../../assets/sun.png';
-import moonImg from '../../assets/moon.png';
+//import sunImg from '../../assets/sun.png';
+//import moonImg from '../../assets/moon.png';
 import '/transitionStyle.css'; // Import the CSS file
 
 //TODO should Steffi / Anna: show timers, inform player if he died, allow players to choose a player to chat to during daytime (insert chat component) Timer needs to be implemented in gameController
@@ -20,9 +16,6 @@ import '/transitionStyle.css'; // Import the CSS file
 //TODO M8 Steffi.	The system shall display the narrator’s script, including all necessary prompts and instructions, on the narrator’s device.
 
 function Game({ownSocketId, messages, setMessages}) {
-    //const [isNight, setIsNight] = useState(false);
-    //const [showDropdown, setShowDropdown] = useState(false);
-    //const [selectedPlayer, setSelectedPlayer] = useState("");
     const [player, setPlayer] = useState(null); // search for id in gamestate after initial fill
     const [phase, setPhase] = useState('day');
     const [showPotions, setShowPotions] = useState(false);
@@ -33,35 +26,6 @@ function Game({ownSocketId, messages, setMessages}) {
     const [votingChoices, setVotingChoices] = useState([]);
     const [votingMsg, setVotingMsg] = useState([]);
     const [modalOpen, setModalOpen] = useState(false);
-
-
-
-    const playersReceivedHandler = (players) => {
-        console.log("Players received:", players);
-        gameController.setPlayers(players);
-        const thisPlayer = gameController.findPlayerById(ownSocketId)
-        playerObject = thisPlayer;
-        console.log(playerObject)
-        setPlayer(thisPlayer);
-        console.log("this player is", player)
-    }
-
-    const phaseReceivedHandler = (phase) => {
-        gameController.setPhase(phase);
-        setPhase(phase);
-        console.log(player);
-    }
-
-
-    const witchShowPotionsHandler = () => {
-        console.log("socket is on")
-        console.log("I am in witchShowPotionsHandler", player)
-        console.log("I am in witchShowPotionsHandler my role is", playerObject.role.roleName)
-        if (playerObject.role.roleName === "Witch") {
-            setShowPotions(true);
-            console.log("Potions should be shown");
-        }
-    }
 
     useEffect(() => {
         // Listen for incoming messages
@@ -96,6 +60,24 @@ function Game({ownSocketId, messages, setMessages}) {
         };
     }, []);
 
+    const playersReceivedHandler = (players) => {
+        gameController.setPlayers(players);
+        const thisPlayer = gameController.findPlayerById(ownSocketId)
+        playerObject = thisPlayer;
+        setPlayer(thisPlayer);
+    };
+
+    const phaseReceivedHandler = (phase) => {
+        gameController.setPhase(phase);
+        setPhase(phase);
+    };
+
+
+    const witchShowPotionsHandler = () => {
+        if (playerObject.role.roleName === "Witch") {
+            setShowPotions(true);
+        }
+    };
 
     const handlePotionUse = (potionType) => {
         if (potionType === "healing" && !healingUsed) {
@@ -107,31 +89,18 @@ function Game({ownSocketId, messages, setMessages}) {
         }
     };
 
-
-    socket.on('showRole', (revealedPlayer) =>{
-        alert(`${revealedPlayer.name}'s Role is: ${revealedPlayer.role.roleName}`)
-    });
-
-
-
-    /*
-        if (!player.isAlive) {
-                alert("You have died!");
-            }
-        }, [player.isAlive]);
-    */
     const handleShowRole = () => {
         setModalOpen(true);
     };
-    const startVotingHandler = ({choices, txtMsg}) =>{
+
+    const startVotingHandler = ({choices, txtMsg}) => {
         console.log("Choices are ", choices)
         setVotingChoices(choices);
         setVoting(true);
         setVotingMsg(txtMsg);
         console.log("votingstate: ", voting);
 
-    }
-
+    };
 
     return (
         <div
@@ -142,65 +111,25 @@ function Game({ownSocketId, messages, setMessages}) {
             </div>
 
             <div
-                className={`flex flex-col items-center justify-center w-full flex-grow py-8 px-4 ${
-                    gameController.getPhase() === "day" ? "bg-white text-black" : "bg-gray-900 text-white"
-                }`}
-            >
+                className={`flex flex-col items-center justify-center w-full min-h-screen transition-colors ${phase === "day" ? "bg-blue-300" : "bg-gray-800"}`}>
+                <div className="relative w-full h-1/2">
+                    {/* {phase === "day" ? (
+                        {/* <img
+                            src={sunImg}
+                            alt="Sun"
+                            className="transition-transform duration-1000 transform sun-animation"
+                        />
+                    ) : (
+                        <img
+                            src={moonImg}
+                            alt="Moon"
+                            className="transition-transform duration-1000 transform moon-animation"
+                        />
+                    ) */}
+                </div>
                 <h1 className="text-4xl font-bold mb-8">
-                    {gameController.getPhase()}!
+                    It is {phase}!
                 </h1>
-            className={`flex flex-col items-center justify-center min-h-screen transition-colors ${phase === "day" ? "bg-blue-300" : "bg-gray-800"}`}>
-            <div className="relative w-full h-1/2">
-                {phase === "day" ? (
-                    <img
-                        src={sunImg}
-                        alt="Sun"
-                        className="transition-transform duration-1000 transform sun-animation"
-                    />
-                ) : (
-                    <img
-                        src={moonImg}
-                        alt="Moon"
-                        className="transition-transform duration-1000 transform moon-animation"
-                    />
-                )}
-            </div>
-            <h1 className="text-4xl font-bold mb-8">
-                It is {phase}!
-            </h1>
-
-            <button
-                onClick={handleShowRole}
-                className="px-6 py-3 text-lg text-white bg-blue-600 rounded-lg transition-colors hover:bg-blue-700"
-            >
-                Show Role
-            </button>
-                {showDropdown && (
-                    <select
-                        value={selectedPlayer}
-                        onChange={(e) => setSelectedPlayer(e.target.value)}
-                        className="border border-gray-300 rounded px-4 py-2 mb-4 text-black"
-                    >
-                        <option value="" disabled>
-                            Select a player
-                        </option>
-                        {gameController.getPlayers()
-                            .filter((foe) => foe.name !== player.name) // Exclude self
-                            .map((foe) => (
-                                <option key={foe.id} value={foe.name}>
-                                    {foe.name}
-                                </option>
-                            ))}
-
-                    </select>
-                )}
-
-                <button
-                    onClick={handleVoteClick}
-                    className="px-6 py-3 text-lg text-white bg-blue-600 rounded-md transition-colors hover:bg-blue-700"
-                >
-                    {showDropdown ? "Send Vote" : "Vote"}
-                </button>
 
                 <button
                     onClick={handleShowRole}
@@ -238,32 +167,13 @@ function Game({ownSocketId, messages, setMessages}) {
                     </div>
                 )}
 
-
-                {player && (
-                    <div>
-                        <PlayerOverview player={player} messages={messages} setMessages={setMessages}/>
-                    </div>
-                )}
+                {player && <PlayerOverview player={player} messages={messages} setMessages={setMessages}/>}
+                {voting &&
+                    <Voting player={player} votingChoices={votingChoices} votingMsg={votingMsg} setVoting={setVoting}/>}
+                {modalOpen && <ModalOverview player={player} onClose={() => setModalOpen(false)}/>}
             </div>
-</div>
-)
-    ;
-            {player && (
-                <div>
-                    <PlayerOverview player={player} messages={messages} setMessages={setMessages}/>
-                </div>
-            )}
-
-            {voting && (
-                <Voting player={player} votingChoices={votingChoices} votingMsg={votingMsg} setVoting={setVoting}/>
-            )}
-
-            {modalOpen && (
-                <ModalOverview player={player} onClose={() => setModalOpen(false)}/>
-            )}
         </div>
     );
-
 }
 
 Game.propTypes = {
--- client/src/pages/Narrator/narrator.jsx
+++ client/src/pages/Narrator/narrator.jsx
@@ -1,13 +1,10 @@
-import {useState, useEffect} from "react";
+import React, {useState, useEffect} from "react";
 import gameController from "../Game/gamelogic/gameController.js";
 import socket from "../../utils/socket.js";
 import PropTypes from "prop-types";
-import Join from "../Join/join.jsx";
-import Player from "../Game/gamelogic/Player.js";
 import PlayerOverview from "../../components/playerOverview.jsx";
-import game from "../Game/game.jsx";
-import sunImg from '../../assets/sun.png';
-import moonImg from '../../assets/moon.png';
+//import //sunImg from '../../assets/sun.png';
+//import moonImg from '../../assets/moon.png';
 
 //TODO show names of all participants and their roles
 //TODO provide instructions for phases
@@ -20,7 +17,6 @@ import moonImg from '../../assets/moon.png';
 function Narrator({joinedLobbyParticipants, selectedRoles}) {
     const [votes, setVotes] = useState({});
     const [currentPhase, setCurrentPhase] = useState(gameController.getPhase());
-    const [sessionID, setSessionID] = useState(gameController.getSessionID());
 
     const wait = async (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
@@ -84,6 +80,7 @@ function Narrator({joinedLobbyParticipants, selectedRoles}) {
 
 
     const startVoting = () => {
+        console.log("startVoting has been called");
         const players = gameController.getPlayers();
         const voters = players.filter(player => player.role.roleName === "Werewolf");
         const choices = players.filter(player => player.role.roleName !== "Werewolf");
@@ -96,37 +93,11 @@ function Narrator({joinedLobbyParticipants, selectedRoles}) {
 
     return (
         <div
-            className={`flex flex-col items-center justify-center min-h-screen transition-colors ${
-                gameController.getPhase() === "day" ? "bg-white text-black" : "bg-gray-900 text-white"
-            }`}
-        >
-            <h1 className="text-4xl font-bold mb-8">
-                It&#39;s {gameController.getPhase()}!
-            </h1>
-            {Object.entries(votes).map(([voter, selectedPlayer]) => (
-                <p key={voter} className="text-lg">{voter} has selected {selectedPlayer}.</p>
-            ))}
-            <button onClick={startVoting}
-                    className="px-6 py-3 text-lg text-white bg-red-600 rounded-md transition-colors hover:bg-red-700"
-            >
-                Start Voting
-            </button>
-            <button
-                onClick={togglePhase}
-                className="px-6 py-3 text-lg text-white bg-red-600 rounded-md transition-colors hover:bg-red-700"
-            >
-                Switch to {gameController.getPhase() === "day" ? "Night" : "Day"}
-            </button>
-
-            {<div><PlayerOverview player={this}/></div>}
             className="flex overflow-hidden flex-col px-1.5 pb-2 mx-auto w-full h-full text-center text-black bg-yellow-950">
-
             <div
                 className="overflow-visible self-stretch px-8 py-2 text-3xl sm:text-4xl md:text-5xl lg:text-6xl whitespace-nowrap rounded-b-xl border-solid bg-stone-300 border-neutral-500 shadow-[0px_2px_2px_rgba(0,0,0,0.25)] font-metal">
                 Wer?Wolf
             </div>
-
-
             <div
                 className={`flex flex-col sm:flex-row w-full flex-grow p-4 sm:p-6 gap-4 sm:gap-6 ${
                     gameController.getPhase() === "day" ? "bg-white text-black" : "bg-gray-900 text-white"
@@ -138,45 +109,40 @@ function Narrator({joinedLobbyParticipants, selectedRoles}) {
                     <h2 className="text-lg sm:text-xl font-bold mb-4 text-black">Script</h2>
                 </div>
 
-
-                {Object.entries(votes).map(([voter, selectedPlayer]) => (
-                    <p key={voter} className="text-lg">
-                        {voter} has selected {selectedPlayer}.
-                    </p>
-                ))}
-
                 {/* Player Overview Component*/}
                 <div className="w-full sm:w-1/3 lg:w-1/4 p-4 overflow-hidden"><PlayerOverview player={this}/>
                 </div>
 
-            </div>
-            <footer className="flex justify-between p-4 sm:p-6">
-                <button
-                    //TODO: add end game functionality
-                    className="w-1/3 sm:w-1/4 lg:w-1/5 px-4 sm:px-6 py-2 sm:py-3 text-sm sm:text-lg bg-orange-200 text-black rounded-md hover:bg-orange-300 transition-all"
-                >
-                    End Game
-                </button>
+                {Object.entries(votes).map(([voter, selectedPlayer]) => (
+                    <p key={voter} className="text-lg">{voter} has selected {selectedPlayer}.</p>
+                ))}
 
-                <button
-                    onClick={startVoting}
-                    className="w-1/3 sm:w-1/4 lg:w-1/5 px-4 sm:px-6 py-2 sm:py-3 text-sm sm:text-lg bg-orange-200 text-black rounded-md hover:bg-orange-300 transition-all"
-                >
-                    Start Voting
-                </button>
+                </div>
+                <footer className="flex justify-between p-4 sm:p-6">
+                    <button
+                        //TODO: add end game functionality
+                        className="w-1/3 sm:w-1/4 lg:w-1/5 px-4 sm:px-6 py-2 sm:py-3 text-sm sm:text-lg bg-orange-200 text-black rounded-md hover:bg-orange-300 transition-all"
+                    >
+                        End Game
+                    </button>
 
-                <button
-                    onClick={togglePhase}
-                    className="w-1/3 sm:w-1/4 lg:w-1/5 px-4 sm:px-6 py-2 sm:py-3 text-sm sm:text-lg bg-orange-200 text-black rounded-md hover:bg-orange-300 transition-all"
-                >
-                    Next Phase
-                </button>
-            </footer>
+                    <button
+                        onClick={startVoting}
+                        className="w-1/3 sm:w-1/4 lg:w-1/5 px-4 sm:px-6 py-2 sm:py-3 text-sm sm:text-lg bg-orange-200 text-black rounded-md hover:bg-orange-300 transition-all"
+                    >
+                        Start Voting
+                    </button>
 
-
+                    <button
+                        onClick={togglePhase}
+                        className="w-1/3 sm:w-1/4 lg:w-1/5 px-4 sm:px-6 py-2 sm:py-3 text-sm sm:text-lg bg-orange-200 text-black rounded-md hover:bg-orange-300 transition-all"
+                    >
+                        Switch to {gameController.getPhase() === "day" ? "Night" : "Day"}
+                    </button>
+                </footer>
         </div>
 
-    )
+    );
 }
 
 Narrator.propTypes = {
